c42b92d2dc49bf56551da4e1151af87495fd0eb6
gpelincel <gabrielpelincel@gmail.com>
2025-07-31 09:49:07 -0300

    Refactor: Cadastro de OS

--- app/Http/Controllers/OrdemServicoController.php
+++ app/Http/Controllers/OrdemServicoController.php
@@ -7,6 +7,7 @@ use App\Http\Requests\StoreOrdemServicoRequest;
 use App\Http\Resources\OrdemServicoResource;
 use App\Services\ClassificacaoOSService;
 use App\Services\ClienteService;
+use App\Services\EquipamentoService;
 use App\Services\OrdemServicoService;
 use App\Services\StatusOSService;
 use App\Services\UnidadeService;
@@ -21,13 +22,15 @@ class OrdemServicoController extends Controller {
     private $statusService;
     private $classificacaoService;
     private $unidadeService;
+    private $equipamentoService;
 
-    public function __construct(OrdemServicoService $osService, ClienteService $clienteService, StatusOSService $statusService, ClassificacaoOSService $classificacaoService, UnidadeService $unidadeService) {
+    public function __construct(OrdemServicoService $osService, ClienteService $clienteService, StatusOSService $statusService, ClassificacaoOSService $classificacaoService, UnidadeService $unidadeService, EquipamentoService $equipamentoService) {
         $this->osService = $osService;
         $this->clienteService = $clienteService;
         $this->statusService = $statusService;
         $this->classificacaoService = $classificacaoService;
         $this->unidadeService = $unidadeService;
+        $this->equipamentoService = $equipamentoService;
     }
 
     /**
@@ -103,10 +106,37 @@ class OrdemServicoController extends Controller {
      * Store a newly created resource in storage.
      */
     public function store(StoreOrdemServicoRequest $request) {
-        $ordem = $this->osService->save($request->all());
+
+        $ordemServico = $request->all();
+
+        if (isset($ordemServico['nota_fiscal'])) {
+            $ordemServico['id_status'] = 2;
+        }
+
+        if (isset($ordemServico['novo-eqp']) && $ordemServico['novo-eqp'] == "1") {
+            $equipamento['numero_serie'] = $ordemServico['numero_serie'];
+            $equipamento['numero_patrimonio'] = $ordemServico['numero_patrimonio'];
+            $equipamento['nome'] = $ordemServico['nome'];
+            $equipamento['id_cliente'] = $ordemServico['id_cliente'];
+            $ordemServico['equipamento'] = $equipamento;
+        }
+
+        if (isset($ordemServico['qtd_1']) && $ordemServico['qtd_1'] > 0) {
+            $ordemServico['itens'] = [];
+
+            for ($i = 1; $i <= $ordemServico['item_counter']; $i++) {
+                $item['quantidade'] = $ordemServico['qtd_' . $i];
+                $item['nome'] = $ordemServico['nome_item_' . $i];
+                $item['valor_unitario'] = $ordemServico['valor_un_' . $i];
+                $item['id_unidade'] = $ordemServico['id_unidade_' . $i];
+                array_push($ordemServico['itens'], $item);
+            }
+        }
+
+        $ordemServico = $this->osService->save($ordemServico);
 
         if (request()->wantsJson()) {
-            return response()->json($ordem);
+            return response()->json($ordemServico);
         }
 
         return redirect()->back()->with('status', 'success')->with('message', 'OS cadastrada com sucesso!');
--- app/Services/OrdemServicoService.php
+++ app/Services/OrdemServicoService.php
@@ -3,9 +3,18 @@
 namespace App\Services;
 
 use App\Models\OrdemServico;
-use DateTime;
+use Illuminate\Support\Facades\Date;
 
 class OrdemServicoService {
+
+    private $itemService;
+    private $equipamentoService;
+
+    public function __construct(ItemService $itemService, EquipamentoService $equipamentoService) {
+        $this->itemService = $itemService;
+        $this->equipamentoService = $equipamentoService;
+    }
+
     public function findByID(string $id) {
         return OrdemServico::with(['equipamento', 'cliente.endereco', 'status', 'classificacao', 'items.unidade'])->findOrFail($id);
     }
@@ -22,58 +31,17 @@ class OrdemServicoService {
 
     public function save(array $ordemServico) {
 
-        extract($ordemServico);
-
-        if (!empty($data_conclusao)) {
-            $data = DateTime::createFromFormat('d/m/Y', $data_conclusao);
-            if ($data) {
-                $ordemServico['data_conclusao'] = $data->format('Y-m-d');
-            }
-        }
-
-        if (!empty($data_inicio)) {
-            $data = DateTime::createFromFormat('d/m/Y', $data_inicio);
-            if ($data) {
-                $ordemServico['data_inicio'] = $data->format('Y-m-d');
-            }
-        }
-
-        if ($id_status == 0) {
-            $id_status = null;
-        }
-
-        if (isset($nota_fiscal)) {
-            $ordemServico['id_status'] = 2;
-        }
-
-        if (isset($ordemServico['novo-eqp']) && $ordemServico['novo-eqp'] == "1") {
-            $equipamentoService = new EquipamentoService();
-            $equipamentoNovo = $equipamentoService->save($ordemServico);
-            $ordemServico['id_equipamento'] = $equipamentoNovo['id'];
+        if (isset($ordemServico['equipamento'])) {
+            $equipamento = $this->equipamentoService->save($ordemServico['equipamento']);
+            $ordemServico['id_equipamento'] = $equipamento->id;
         }
 
         $ordemReturn = OrdemServico::create($ordemServico);
 
-        if (isset($ordemServico['qtd_1']) && $ordemServico['qtd_1'] > 0) {
-            for ($i = 1; $i <= $item_counter; $i++) {
-                $item['quantidade'] = $ordemServico['qtd_' . $i];
-                $item['nome'] = $ordemServico['nome_item_' . $i];
-                $item['valor_unitario'] = $ordemServico['valor_un_' . $i];
-                $item['id_unidade'] = $ordemServico['id_unidade_' . $i];
+        if (isset($ordemServico['itens'])) {
+            foreach ($ordemServico['itens'] as $item) {
                 $item['id_os'] = $ordemReturn->id;
-
-                $itemService = new ItemService();
-                $itemService->save($item);
-            }
-        }
-
-        if (isset($itens)) {
-            foreach ($itens as $item) {
-                $itemService = new ItemService();
-                if ($item['quantidade']) {
-                    $item['id_os'] = $ordemReturn->id;
-                    $itemService->save($item);
-                }
+                $this->itemService->save($item);
             }
         }
 
@@ -92,14 +60,14 @@ class OrdemServicoService {
 
         $ordemServico = OrdemServico::findOrFail($id);
         if (!empty($ordemServico['data_conclusao'])) {
-            $data = DateTime::createFromFormat('d/m/Y', $ordemServico['data_conclusao']);
+            $data = Date::createFromFormat('d/m/Y', $ordemServico['data_conclusao']);
             if ($data) {
                 $novoOrdemServico['data_conclusao'] = $data->format('Y-m-d');
             }
         }
 
         if (!empty($ordemServico['data_inicio'])) {
-            $data = DateTime::createFromFormat('d/m/Y', $ordemServico['data_inicio']);
+            $data = Date::createFromFormat('d/m/Y', $ordemServico['data_inicio']);
             if ($data) {
                 $novoOrdemServico['data_inicio'] = $data->format('Y-m-d');
             }
